07d1566827ae3b2600c5c5683e069e4354b8d9b8
ShenYj <shenyj4@51nb.com>
2017-10-27 17:05:26 +0800

    new

---
+++ Ecg2GForPad/Ecg2GForPad/SupportingFiles/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ecg2GForPad
+//
+//  Created by ecg on 2017/10/27.
+//  Copyright © 2017年 Auko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
